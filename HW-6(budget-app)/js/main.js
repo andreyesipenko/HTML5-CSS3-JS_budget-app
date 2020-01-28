ff9ac81b2c1f19798161022f160d4748b5c5d786
Andrey Esipenko <mralcapone96@gmail.com>
2020-01-28 15:55:05 +0200

    getElement(-s)ByClassName/TagName/Id/querySelector...

---
+++ HW-6(budget-app)/js/main.js
@@ -0,0 +1,34 @@
+let start = document.getElementById('start'),
+
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value'),
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+
+    expensesItemBtn = document.getElementsByTagName('button')[0],
+    optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBudgetBtn = document.getElementsByTagName('button')[2],
+
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+
+    incomeItem = document.querySelector('.choose-income'),
+    savingsItem = document.querySelector('#savings'),
+    sumItem = document.querySelector('.choose-sum'),
+    percentItem = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+
+
+
+
+
+
+
